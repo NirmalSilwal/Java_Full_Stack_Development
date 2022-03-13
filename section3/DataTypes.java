d1671a7082b9c09fa1d1ab14b0b46f012d0ded1f
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-13 18:31:10 -0500

    various data types in java and usage

---
+++ section3/DataTypes.java
@@ -0,0 +1,45 @@
+package section3;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+
+		int n = 1;
+		float f = 1;
+		double d = 1;
+
+		System.out.println("integer value is " + n);
+		System.out.println("float value is " + f);
+		System.out.println("double value is " + d);
+
+		float ff = 1f;
+		double dd = 1d;
+
+		System.out.println("float value is " + ff);
+		System.out.println("double value is " + dd);
+
+		System.out.println();
+
+		System.out.println(9 / 2);
+		System.out.println(9f / 2f);
+		System.out.println(9d / 2d);
+
+		System.out.println();
+		System.out.println(10f / 6f);
+		System.out.println(10d / 6d);
+
+		System.out.println();
+		System.out.println(10f / 6);
+		System.out.println(10d / 6);
+
+		System.out.println();
+		System.out.println(10 / 6f);
+		System.out.println(10 / 6d);
+
+		// unicode character - https://unicode-table.com/
+		System.out.println();
+		System.out.println('§');
+		
+		
+	}
+}
