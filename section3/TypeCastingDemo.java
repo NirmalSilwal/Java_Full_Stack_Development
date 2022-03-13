ef074cac55001851cde55d52705e7d8774796f84
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-13 18:31:26 -0500

    shows how type casting works in java

---
+++ section3/TypeCastingDemo.java
@@ -0,0 +1,21 @@
+package section3;
+
+public class TypeCastingDemo {
+
+	public static void main(String[] args) {
+
+		// type casting means conversion of one data type to another data type
+
+		short sh = 5;
+		int n = sh; // automatic type casting
+
+		System.out.println(n);
+
+		int y = 3;
+//		short x =  y; // Type mismatch: cannot convert from int to short
+		
+		// widening and narrowing concepts
+		
+		
+	}
+}
