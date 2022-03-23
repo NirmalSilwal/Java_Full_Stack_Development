2973fb57ad7e7159caea5c82589651659fd3fe93
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-03-23 05:51:06 -0500

    Demo for using lambda expressions, various case

---
+++ section16/Lambda2.java
@@ -0,0 +1,27 @@
+package section16;
+
+public class Lambda2 {
+
+	public static void main(String[] args) {
+
+		Thread thread1 = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("I am inside thread1");
+			}
+
+		});
+
+		thread1.setName("First thread");
+		thread1.start();
+
+		System.out.println(thread1.getName());
+
+		// now using lambda expression
+
+		Thread thread2 = new Thread(() -> System.out.println("inside lambda thread2"));
+
+		thread2.start();
+	}
+}
